a5303509b53fb29418c15b4156cc56a4e2ab8f2c
Stefan Evans <stefan.evans@consegna.cloud>
2022-05-08 11:16:47 +1200

    added vpc c

--- main.tf
+++ main.tf
@@ -22,11 +22,11 @@ module "vpc_b" {
     VPC_PRIVATE_SUBNET_NAME = "stef_vpc_b_subnet_private_terraform"    
 }
 
-# module "vpc_c" {
-#     source = "./modules/vpc"
-#     VPC_CIDR = "10.2.0.0/16"
-#     VPC_NAME = "stef_vpc_c_terraform"
+module "vpc_c" {
+    source = "./modules/vpc"
+    VPC_CIDR = "10.2.0.0/16"
+    VPC_NAME = "stef_vpc_c_terraform"
 
-#     VPC_PRIVATE_SUBNET_CIDR = "10.2.0.0/24"
-#     VPC_PRIVATE_SUBNET_NAME = "stef_vpc_c_subnet_private_terraform"    
-# }
+    VPC_PRIVATE_SUBNET_CIDR = "10.2.0.0/24"
+    VPC_PRIVATE_SUBNET_NAME = "stef_vpc_c_subnet_private_terraform"    
+}
